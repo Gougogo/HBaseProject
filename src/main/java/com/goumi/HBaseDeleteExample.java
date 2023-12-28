024876dfed24783afe2453218fc106b1f0f033a1
Gougogo <1572158030@qq.com>
2023-12-28 11:19:27 +0800

    commit.

---
+++ src/main/java/com/goumi/HBaseDeleteExample.java
@@ -0,0 +1,59 @@
+package com.goumi;
+
+/**
+ * @version 1.0
+ * @auther GouMi
+ */
+import com.goumi.utils.HBaseConnection;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.filter.CompareFilter;
+import org.apache.hadoop.hbase.filter.SingleColumnValueFilter;
+import org.apache.hadoop.hbase.util.Bytes;
+
+public class HBaseDeleteExample {
+    public static Connection connection = HBaseConnection.connection;
+    public static void main(String[] args) {
+        try {
+            // 创建表名对象
+            TableName tableName = TableName.valueOf("student");
+
+            // 创建表实例
+            Table table = connection.getTable(tableName);
+
+            // 创建扫描器对象
+            Scan scan = new Scan();
+
+            // 添加列族和列的过滤器
+            scan.addColumn(Bytes.toBytes("info"), Bytes.toBytes("bs"));
+            scan.setFilter(new SingleColumnValueFilter(
+                    Bytes.toBytes("info"),
+                    Bytes.toBytes("bs"),
+                    CompareFilter.CompareOp.EQUAL,
+                    Bytes.toBytes("03")
+            ));
+
+            // 获取扫描结果的迭代器
+            ResultScanner scanner = table.getScanner(scan);
+
+            // 遍历扫描结果并删除对应的行
+            for (Result result : scanner) {
+                System.out.println(Bytes.toString(result.getRow()));
+                Delete delete = new Delete(result.getRow());
+                table.delete(delete);
+            }
+
+            // 关闭资源
+            scanner.close();
+            table.close();
+            connection.close();
+
+            System.out.println("删除操作完成。");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
